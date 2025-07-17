a539fc720da82514501b544330b2e9f11bc1cb84
rebelzi <firdauschuzaeni@gmail.com>
2025-07-17 22:04:29 +0700

    feat: login

---
+++ lib/pages/on_boarding/auth/login_page.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:resepin/theme/appColors.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obsecurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Padding(
+        padding: EdgeInsets.only(top: height * 0.1),
+        child: Center(
+          child: Column(
+            children: [
+              // header
+              Text(
+                "Log in",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: height * 0.025),
+              Text(
+                "Silahkan Masukkan Email dan Password anda",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: height * 0.07),
+
+              //batas header
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(47),
+                      topRight: Radius.circular(47),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * 0.07),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // email
+                        Text(
+                          "Email",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: height * 0.01),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.grey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              hintText: "Masukkan Email",
+                              hintStyle: GoogleFonts.poppins(
+                                color: Colors.grey,
+                                fontSize: 14,
+                              ),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 30,
+                                horizontal: 10,
+                              ),
+                            ),
+                          ),
+                        ),
+                        // end email
+                        SizedBox(height: height * 0.02),
+                        // password
+                        Text(
+                          "Password",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: height * 0.01),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.grey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: _obsecurePassword,
+                            decoration: InputDecoration(
+                              hintText: "Masukkan Password",
+                              hintStyle: GoogleFonts.poppins(
+                                color: Colors.grey,
+                                fontSize: 14,
+                              ),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 30,
+                                horizontal: 10,
+                              ),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _obsecurePassword = !_obsecurePassword;
+                                  });
+                                },
+                                icon: Icon(
+                                  _obsecurePassword
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  size: 26,
+                                ),
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        // end password
+                        SizedBox(height: height * 0.02),
+
+                        // button login
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              print("login");
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.primary,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.symmetric(vertical: 20),
+                            ),
+                            child: Text(
+                              "Login",
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: height * 0.02),
+                        // button register
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Belum punya akun?",
+                                style: GoogleFonts.poppins(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+
+                              GestureDetector(
+                                // onTap: () {
+                                //   Navigator.pushNamed(context, '/register');
+                                // },
+                                child: Text(
+                                  " Sign Up",
+                                  style: GoogleFonts.poppins(
+                                    color: AppColors.primary,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme/appColors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFFFE724C);
+  static const Color grey = Color(0xFFF5F5F5);
+}
